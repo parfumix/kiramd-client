df9e4adccdb5dfd698f16677ad94ec94d0226933
Radu <radu.bordei89@gmail.com>
2021-07-30 20:39:04 +0300

    no message

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,13 +1,64 @@
 <template>
-  
+    <nav>
+        <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+        <NuxtLink
+            v-for="({ title, icon, path }, $index) in items"
+            :class="{
+                'bg-gray-900 text-white': $route.path == path,
+                'text-gray-300 hover:bg-gray-700 hover:text-white': $route.path != path,
+            }"
+            :key="$index"
+            :to="path"
+            class="
+                group
+                flex
+                items-center
+                px-2
+                py-2
+                text-sm
+                font-medium
+                rounded-md
+            "
+        >
+            <span :class="{
+                'text-gray-300': $route.path == path,
+                'text-gray-400 group-hover:text-gray-300': $route.path != path,
+            }" v-html="icon"></span>
+            <!-- Current: "text-gray-300", Default: "text-gray-400 group-hover:text-gray-300" -->
+            {{ title }}
+        </NuxtLink>
+    </nav>
 </template>
 
 <script>
 export default {
-
-}
+    data() {
+        return {
+            items: [{
+                icon: '<svg class="text-gray-300 mr-3 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>',
+                title: 'Chiriile mele',
+                path: '/',
+            },{
+                icon: '<svg class="text-gray-300 mr-3 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>',
+                title: 'Semneaza contract',
+                path: '/sign-contract',
+            },{
+                icon: '<svg class="text-gray-300 mr-3 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>',
+                title: 'Asigura chiria',
+                path: '/insurance',
+            },{
+                icon: '<svg class="text-gray-300 mr-3 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>',
+                title: 'Mesajele mele',
+                path: '/messages',
+            },{
+                icon: '<svg class="text-gray-300 mr-3 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>',
+                title: 'Tranzactii',
+                path: '/transactions',
+            }]
+        }
+    }
+};
 </script>
 
 <style>
-
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -15,16 +15,11 @@
             </div>
             <div class="flex-1 h-0 pt-5 pb-4 overflow-y-auto">
               <div class="flex-shrink-0 flex items-center px-4">
-                <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow">
+                <NuxtLink to="/">
+                  <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow">
+                </NuxtLink>
               </div>
-              <nav class="mt-5 px-2 space-y-1">
-                  <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                  <NuxtLink v-for="({ title, icon, path }, $index) in items" :key="$index" :to="path" class="bg-gray-900 text-white group flex items-center px-2 py-2 text-base font-medium rounded-md">
-                    <!-- Current: "text-gray-300", Default: "text-gray-400 group-hover:text-gray-300" -->
-                    <span v-html="icon"></span>
-                    {{ title }}
-                  </NuxtLink>
-              </nav>
+              <Sidebar class="mt-5 px-2 space-y-1" />
             </div>
             <div class="flex-shrink-0 flex bg-gray-700 p-4">
               <a href="#" class="flex-shrink-0 group block">
@@ -57,16 +52,11 @@
           <div class="flex flex-col h-0 flex-1 bg-gray-800">
             <div class="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
               <div class="flex items-center flex-shrink-0 px-4">
-                <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow">
-              </div>
-              <nav class="mt-5 flex-1 px-2 bg-gray-800 space-y-1">
-                <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                <NuxtLink v-for="({ title, icon, path }, $index) in items" :key="$index" :to="path" class="bg-gray-900 text-white group flex items-center px-2 py-2 text-sm font-medium rounded-md">
-                  <span v-html="icon"></span>
-                  <!-- Current: "text-gray-300", Default: "text-gray-400 group-hover:text-gray-300" -->
-                  {{ title }}
+                <NuxtLink to="/">
+                  <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow">
                 </NuxtLink>
-              </nav>
+              </div>
+              <Sidebar class="mt-5 flex-1 px-2 bg-gray-800 space-y-1" />
             </div>
             <div class="flex-shrink-0 flex bg-gray-700 p-4">
               <a href="#" class="flex-shrink-0 w-full group block">
@@ -106,11 +96,6 @@ export default {
   data() {
     return {
       sidebarOpen: false,
-      items: [{
-        icon: '<svg class="text-gray-300 mr-3 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>',
-        title: 'Dashboard',
-        path: '/',
-      }]
     }
   }
 }
