af0bdca72626698e9ab7de51d09350b34cbe375c
Radu <radu.bordei89@gmail.com>
2021-07-30 20:14:07 +0300

    no message

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,62 +1,120 @@
 <template>
-  <div>
-    <Nuxt />
+ <div class="bg-gray-100">
+    <div class="h-screen flex overflow-hidden bg-gray-100" @keydown.escape="sidebarOpen = false">
+      <div v-if="sidebarOpen" class="md:hidden">
+        <div class="fixed inset-0 flex z-40">
+          <transition enter-active-class="transition-opacity ease-linear duration-300" enter-class="opacity-0" enter-to-class="opacity-100" leave-active-class="transition-opacity ease-linear duration-300" leave-class="opacity-100" leave-to-class="opacity-0"><div @click="sidebarOpen = false" v-if="sidebarOpen" class="fixed inset-0">
+            <div class="absolute inset-0 bg-gray-600 opacity-75"></div>
+          </div></transition>
+          <transition enter-active-class="transition ease-in-out duration-300 transform" enter-class="-translate-x-full" enter-to-class="translate-x-0" leave-active-class="transition ease-in-out duration-300 transform" leave-class="translate-x-0" leave-to-class="-translate-x-full"><div v-if="sidebarOpen" class="relative flex-1 flex flex-col max-w-xs w-full bg-gray-800">
+            <div class="absolute top-0 right-0 -mr-12 pt-2">
+              <button v-if="sidebarOpen" @click="sidebarOpen = false" class="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <span class="sr-only">Close sidebar</span>
+                <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
+              </button>
+            </div>
+            <div class="flex-1 h-0 pt-5 pb-4 overflow-y-auto">
+              <div class="flex-shrink-0 flex items-center px-4">
+                <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow">
+              </div>
+              <nav class="mt-5 px-2 space-y-1">
+                  <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+                  <NuxtLink v-for="({ title, icon, path }, $index) in items" :key="$index" :to="path" class="bg-gray-900 text-white group flex items-center px-2 py-2 text-base font-medium rounded-md">
+                    <!-- Current: "text-gray-300", Default: "text-gray-400 group-hover:text-gray-300" -->
+                    <span v-html="icon"></span>
+                    {{ title }}
+                  </NuxtLink>
+              </nav>
+            </div>
+            <div class="flex-shrink-0 flex bg-gray-700 p-4">
+              <a href="#" class="flex-shrink-0 group block">
+                <div class="flex items-center">
+                  <div>
+                    <img class="inline-block h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80" alt="">
+                  </div>
+                  <div class="ml-3">
+                    <p class="text-base font-medium text-white">
+                      Tom Cook
+                    </p>
+                    <p class="text-sm font-medium text-gray-400 group-hover:text-gray-300">
+                      View profile
+                    </p>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div></transition>
+          <div class="flex-shrink-0 w-14">
+            <!-- Force sidebar to shrink to fit close icon -->
+          </div>
+        </div>
+      </div>
+
+      <!-- Static sidebar for desktop -->
+      <div class="hidden md:flex md:flex-shrink-0">
+        <div class="flex flex-col w-64">
+          <!-- Sidebar component, swap this element with another sidebar if you like -->
+          <div class="flex flex-col h-0 flex-1 bg-gray-800">
+            <div class="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
+              <div class="flex items-center flex-shrink-0 px-4">
+                <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow">
+              </div>
+              <nav class="mt-5 flex-1 px-2 bg-gray-800 space-y-1">
+                <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+                <NuxtLink v-for="({ title, icon, path }, $index) in items" :key="$index" :to="path" class="bg-gray-900 text-white group flex items-center px-2 py-2 text-sm font-medium rounded-md">
+                  <span v-html="icon"></span>
+                  <!-- Current: "text-gray-300", Default: "text-gray-400 group-hover:text-gray-300" -->
+                  {{ title }}
+                </NuxtLink>
+              </nav>
+            </div>
+            <div class="flex-shrink-0 flex bg-gray-700 p-4">
+              <a href="#" class="flex-shrink-0 w-full group block">
+                <div class="flex items-center">
+                  <div>
+                    <img class="inline-block h-9 w-9 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=2&amp;w=256&amp;h=256&amp;q=80" alt="">
+                  </div>
+                  <div class="ml-3">
+                    <p class="text-sm font-medium text-white">
+                      Tom Cook
+                    </p>
+                    <p class="text-xs font-medium text-gray-300 group-hover:text-gray-200">
+                      View profile
+                    </p>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-col w-0 flex-1 overflow-hidden">
+        <div class="md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3">
+          <button @click.stop="sidebarOpen = true" class="-ml-0.5 -mt-0.5 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+            <span class="sr-only">Open sidebar</span>
+            <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+          </button>
+        </div>
+        <Nuxt />
+      </div>
+    </div>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      sidebarOpen: false,
+      items: [{
+        icon: '<svg class="text-gray-300 mr-3 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path></svg>',
+        title: 'Dashboard',
+        path: '/',
+      }]
+    }
+  }
+}
+</script>
+
 <style>
-html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
 
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-</style>
